6022057936d75525ee84233c1f4aba1abdf292a0
lazzieecoderr <chakravarthyrrc@gmail.com>
2024-04-30 12:38:10 +0530

    base completed

---
+++ README.md
@@ -0,0 +1,2 @@
+# React Day 2
+Here we are seeing the installation and folder structure of react.
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+//import part
+
+const App = () => {
+  //js part
+  const a = 10;
+  return (
+    //html part
+    <>
+      <h1>Hello World</h1>
+      {a}
+    </>
+  );
+};
+
+//export part
+export default App;
